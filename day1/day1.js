614c2edcb2fb68d60efdc4cbe9a924df42ddcc39
Eivind Høiseth-Gilje <eivindgi@gmail.com>
2021-12-01 14:54:37 +0100

    day 1

---
+++ day1/day1.js
@@ -0,0 +1,19 @@
+const fs = require('fs');
+
+const input = fs
+  .readFileSync('input.txt', 'utf8')
+  .split('\n')
+  .map((string) => parseInt(string));
+
+const increaseCount = input.reduce((acc, cur, idx, arr) => (cur > arr[idx - 1] ? acc + 1 : acc), 0);
+console.log(
+  `Part 1: How many measurements are larger than the previous measurement?  ${increaseCount}`,
+);
+
+const increaseCountGrouped = input
+  .map((val, idx, arr) => (arr[idx + 2] ? arr[idx] + arr[idx + 1] + arr[idx + 2] : undefined))
+  .filter((x) => !!x)
+  .reduce((acc, cur, idx, arr) => (cur > arr[idx - 1] ? acc + 1 : acc), 0);
+console.log(`Part 2: How many sums are larger than the previous sum? ${increaseCountGrouped}`);
+
+//cleaner:
