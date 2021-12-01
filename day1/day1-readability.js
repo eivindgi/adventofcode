9825e1b896a5bdb076cabb6f0086cb9fd7b06b9d
Eivind Høiseth-Gilje <eivindgi@gmail.com>
2021-12-01 14:55:41 +0100

    day 1, more readable(?) version

---
+++ day1/day1-readability.js
@@ -0,0 +1,40 @@
+const fs = require('fs');
+
+//helpers
+const readFileOfNumbers = fs
+  .readFileSync('input.txt', 'utf8')
+  .split('\n')
+  .map((string) => parseInt(string));
+
+const toSum = (current, sum) => current + sum;
+
+const sumWindow = (array, startIndex, windowSize) => {
+  const window = array.slice(startIndex, startIndex + windowSize);
+  return window.reduce(toSum);
+};
+
+const toSumsOf = (windowSize) => (val, idx, arr) => {
+  const hasFullWindowLength = arr[idx + windowSize - 1];
+  return hasFullWindowLength ? sumWindow(arr, idx, windowSize) : undefined;
+};
+
+const removeUndefined = (x) => !!x;
+
+const toIncreaseCount = (count, current, index, array) => {
+  const prev = array[index - 1];
+  return current > prev ? count + 1 : count;
+};
+
+//solution
+const input = readFileOfNumbers;
+
+const increaseCount = input.reduce(toIncreaseCount, 0);
+console.log(
+  `Part 1: How many measurements are larger than the previous measurement?  ${increaseCount}`,
+);
+
+const increaseCountSummed = input
+  .map(toSumsOf(3))
+  .filter(removeUndefined)
+  .reduce(toIncreaseCount, 0);
+console.log(`Part 2: How many sums are larger than the previous sum? ${increaseCountSummed}`);
